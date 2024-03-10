05b7517d2c2fea76a40ac1c4cf07b9a2f0fc7a12
Kelley Shackelford <kelleyalexis@yahoo.com>
2024-03-10 17:38:41 -0400

    Learning first-class functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = arrayOfDrivers => arrayOfDrivers.slice(0, 2);
+
+const returnLastTwoDrivers = arrayOfDrivers => arrayOfDrivers.slice(2, 4);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return fareMultiplier = x => x * fare;  
+}
+const fareDoubler = fare => 2 * fare;
+
+const fareTripler = fare => 3 * fare;
+
+const selectDifferentDrivers = (arrayOfDrivers, returnFirstTwoDrivers) => returnFirstTwoDrivers(arrayOfDrivers);
